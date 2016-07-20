5966c48f8f119d28c16099c01418888a9717c1cc
Bruz Marzolf <bruz@bruzilla.com>
2016-07-19 20:32:57 -0700

    Prune commented out code

--- src/components/Groceries.js
+++ src/components/Groceries.js
@@ -57,12 +57,6 @@ export default class Groceries extends Component {
     })
   }
 
-  // componentWillReceiveProps(nextProps) {
-  //   console.log('NEXT PROPS')
-  //   console.log(nextProps)
-
-  // }
-
   renderRow(rowData) {
     console.log(this.props.connected)
     return (
